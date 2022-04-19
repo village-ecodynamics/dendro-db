85b213fb02ea496d92b7c18a66ce04e349172366
Darcy Bird <darcyabird@gmail.com>
2022-04-19 12:02:46 -0700

    added reuven bibs

--- R/AddNewData.R
+++ R/AddNewData.R
@@ -1,3 +1,11 @@
+library(tidyverse)
+library(magrittr)
+library(robinson2020)
+library(villager)
 
+readr::read_csv(here::here("data-derived/TR_2022_04_01.csv")) 
+
+reuven <- readr::read_csv(here::here("data-raw/Reuven_2021_AD200_800_tr.csv"))
+  
 
 
